5a0736e38e4b4cc9047dc4461c82f75d45dd5aa4
drup1234 <drupprajapati01@gmail.com>
2025-03-13 08:17:24 +0530

    Add files via upload

---
+++ Myfirstclass.java
@@ -0,0 +1,11 @@
+public class Myfirstclass
+{
+    public static void main(String[]args)
+    {
+        System.out.print("hello");
+        System.out.print("java");
+        System.out.println();
+        System.out.println("this is my first java program");
+
+    }
+}
